686ee0492f7ecb1b6b5c4eb20800b0d99b2561f8
Konstantin Gogov <konstantin.gogov@gmail.com>
2021-09-17 23:37:47 +0300

    feat: visualize all planets as boxes on the index page

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,6 +1,5 @@
 import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
-
 export default class Application extends EventEmitter {
   static get events() {
     return {
@@ -11,17 +10,58 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
+    this.planets = [];
 
-    document.body.querySelector(".main").appendChild(box);
+    this._loading = document.body.querySelector('progress');
 
-    this.emit(Application.events.READY);
+    this._load().then(() => this.emit(Application.events.READY));
+  }
+
+  async _load() {
+    const URL = 'https://swapi.boom.dev/api/planets/';
+    let next = URL;
+
+    this._startLoading();
+
+    while (next) {
+      const response = await fetch(next);
+
+      if (!response.ok) {
+        throw new Error (`HTTP error! Status: ${response.status}`);
+      }
+
+      const planetCollection = await response.json();
+      next = planetCollection.next;
+
+      this.planets = [...this.planets, ...planetCollection.results];
+    }
+
+    this._create();
+
+    this._stopLoading();
+  }
+
+  _create() {
+    this.planets.forEach(planet => {
+      const box = document.createElement('div');
+      box.classList.add('box');
+
+      box.innerHTML = this._render({
+        name: planet.name,
+        terrain: planet.terrain,
+        population: planet.population,
+      });
+
+      document.body.querySelector(".main").appendChild(box);
+    })
+  }
+
+  _startLoading() {
+    this._loading.style.display = 'block';
+  }
+
+  _stopLoading() {
+    this._loading.style.display = 'none';
   }
 
   _render({ name, terrain, population }) {
